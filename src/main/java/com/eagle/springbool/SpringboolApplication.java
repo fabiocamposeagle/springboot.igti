680184558c3d65153d79faede39d8fe2c8b14ddc
Fabio Campos <eaglebooks77@gmail.com>
2021-11-02 11:29:50 -0300

    Create Project

---
+++ src/main/java/com/eagle/springbool/SpringboolApplication.java
@@ -0,0 +1,13 @@
+package com.eagle.springbool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringboolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eagle/springbool/SpringboolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eagle.springbool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringboolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
